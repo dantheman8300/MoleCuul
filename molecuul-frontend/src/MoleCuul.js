6aeb0e426cb0c7a53ec90fa8919c9e3f4ecf8aeb
Anthony Bui <anthonynbui99@gmail.com>
2022-10-30 23:30:57 -0700

    header + hamburger menu

--- molecuul-frontend/src/MoleCuul.js
+++ molecuul-frontend/src/MoleCuul.js
@@ -2,13 +2,15 @@ import React from "react";
 import ElementTile from "./ElementTile";
 import ElementTool from "./ElementTool";
 import ConfigurationMenu from "./ConfigurationMenu";
+import Header from "./Header";
+import HamburgerMenu from "./HamburgerMenu";
+import Sidebar from "./Sidebar";
 
 function MoleCuul() {
     return (
         <div className="container">
-            <ElementTile />
-            <ElementTool />
-            <ConfigurationMenu />
+            <Header />
+            <HamburgerMenu />
         </div>
     );
 }
