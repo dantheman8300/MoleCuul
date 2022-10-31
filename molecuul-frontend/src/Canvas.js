27541ef8c72977739438425b07ed1ffbec296c9c
Daniel-Astriddao <daniel@astriddao.xyz>
2022-10-30 23:47:31 -0700

    made canvas icons clickable

--- molecuul-frontend/src/Canvas.js
+++ molecuul-frontend/src/Canvas.js
@@ -8,15 +8,32 @@ import elementHex from './icons/Element hex.png';
 
 
 function IconBox () {
+
+  const handleZoomOut = event => {
+    console.log(`zooming out`)
+  }
+
+  const handleZoomIn = event => {
+    console.log(`zooming in`)
+  }
+
+  const handleTrash = event => {
+    console.log('trash')
+  }
+
+  const handleCursor = event => {
+    console.log('cursor')
+  }
+
   return (
     <div className="iconBox">
       <div className='iconRow'>
-        <img src={iconMinus} alt='minus icon' className='icon'/>
-        <img src={iconPlus} alt='plus icon' className='icon'/>  
+        <img src={iconMinus} alt='minus icon' className='icon' onClick={handleZoomOut}/>
+        <img src={iconPlus} alt='plus icon' className='icon' onClick={handleZoomIn}/>  
       </div>
       <div className='iconRow'>
-        <img src={iconCursor} alt='cursor icon' className='icon'/>
-        <img src={iconTrash} alt='trash icon' className='icon'/>  
+        <img src={iconCursor} alt='cursor icon' className='icon' onClick={handleCursor}/>
+        <img src={iconTrash} alt='trash icon' className='icon' onClick={handleTrash}/>  
       </div>
     </div>
   )
@@ -26,9 +43,8 @@ function Canvas () {
   return (
     <div className="canvas">
       <IconBox/>
-      <div className="grid-canvas">
-        Canvas
-        <img src={elementHex} alt='hydrogen'/>
+      <div >
+        <img src={elementHex} alt='hydrogen' className='molecule'/>
       </div>
     </div>
   );
