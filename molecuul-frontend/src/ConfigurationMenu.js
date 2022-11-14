3349efb42ff283305227abec15af1f0c463a41a7
eliseemelia <eliseemelia@gmail.com>
2022-11-14 15:54:30 -0800

    added single menu

--- molecuul-frontend/src/ConfigurationMenu.js
+++ molecuul-frontend/src/ConfigurationMenu.js
@@ -1,12 +1,19 @@
 import React from "react";
+import ConfigMenuTile from "./ConfigMenuTile";
 import ConfigMenuTiles from "./ConfigMenuTiles";
 
 function ConfigurationMenu(props) {
     const images = props.info.configs;
     const symbol = props.info.symbol;
+    const singleConfig = (() => {
+        console.log("hi" + images)
+        if(images.length === 1) return true;
+        else return false;
+    })();
     return (
-        <div className="configurationMenu">
-            <ConfigMenuTiles configs={images} symbol={symbol}/>
+        <div >
+        {singleConfig && <div className="configurationMenu-single"><ConfigMenuTile config={images} symbol={symbol} /> </ div>}
+        {!singleConfig && <div className="configurationMenu"><ConfigMenuTiles configs={images} symbol={symbol}/> </ div>}
         </div>
     )
 }
