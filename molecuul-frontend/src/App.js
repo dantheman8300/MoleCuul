f157c6b5f32803e30aef4a53e0ee25240c3e2655
eliseemelia <eliseemelia@gmail.com>
2022-11-16 13:08:13 -0800

    added intructions

--- molecuul-frontend/src/App.js
+++ molecuul-frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import InstructionTile from "./InstructionTile";
 import Sidebar from "./Sidebar";
 
 function MoleCuul() {
 
+    // const lst = [
+    //     "CH4",
+    //     "C2H6",
+    //     "CO2",
+    //     "H2O",
+    //     "CH2O",
+    //     "C2H2",
+    //     "C2H4O",
+    //     "CH3OH"
+    // ]
     return (
         <div className="container">
             <Sidebar />
+            <InstructionTile />
         </div>
     )
 }
