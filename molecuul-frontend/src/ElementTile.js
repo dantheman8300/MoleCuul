0c1e9d4c41fa673411bdf198667624ffb7f36105
Daniel-Astriddao <daniel@astriddao.xyz>
2022-11-28 23:25:58 -0800

    got rotation to work

--- molecuul-frontend/src/ElementTile.js
+++ molecuul-frontend/src/ElementTile.js
@@ -7,7 +7,10 @@ function ElementTile(props) {
     const image = props.image;
     const symbol = props.symbol;
     const elementId = props.id;
-    const lStructure = props.image.lStructure
+    let lStructure = props.image.lStructure;
+
+    const [rotation, setRotation] = React.useState(0);
+
 
 
     const getElementInfo = () => {
@@ -17,12 +20,39 @@ function ElementTile(props) {
 
     const getElementInfoFromDatabase = (id) => {
         // Get element info from database based on id
-        const elementInfo = { // Todo: get element info from database
+        const elementInfo = { 
             name: symbol, // Pass through the symbol for rendering
-            lStructure: lStructure,
-            source: image.imagefile // Need to attach source to elementInfo for rendering on canvas!
+            lStructure: lStructure.slice(), // Pass through a copy of the lStructure
+            source: image.imagefile, // Need to attach source to elementInfo for rendering on canvas!
+            rotation: rotation % 8
         };
         return elementInfo;
+    }
+
+    const handleRotation = (rotator) => {
+        console.log(`Starting rotation: ${rotation}`);
+        console.log(`Rotation changed with rotator ${rotator}`);
+        if (rotator === 0) {
+            lStructure = props.image.lStructure;
+            setRotation(0);
+        } else {
+            rotateLStructure();
+            setRotation(rotation + rotator);
+        }
+
+        console.log(`Rotation changed to ${rotation}`);
+    }
+
+    const rotateLStructure = () => {
+        let temp = lStructure[7];
+        lStructure[7] = lStructure[6];
+        lStructure[6] = lStructure[5];
+        lStructure[5] = lStructure[4];
+        lStructure[4] = lStructure[3];
+        lStructure[3] = lStructure[2];
+        lStructure[2] = lStructure[1];
+        lStructure[1] = lStructure[0];
+        lStructure[0] = temp;
     }
 
     return (
@@ -31,8 +61,9 @@ function ElementTile(props) {
             draggable 
             onDragStart={getElementInfo}
             onDragEnd={props.handleDragEnd}
+            onClick={() => handleRotation(1)}
         >
-            <Octagon image={image} alt={"image of" + image} />
+            <Octagon image={image} alt={"image of" + image} rotation={rotation} />
             <OctagonSymbol symbol={symbol}/>
         </div>
 
