e316d8008ff93bab47909fdf9565aa509b82ac88
Daniel-Astriddao <daniel@astriddao.xyz>
2022-12-02 11:27:09 -0800

    fixed typos

--- molecuul-frontend/src/tutorial-pages/Page9.js
+++ molecuul-frontend/src/tutorial-pages/Page9.js
@@ -11,7 +11,7 @@ function PageNine (props) {
 
     return (
         <div className="pages">
-        <div className="text-box" id="page-nine-box">x means you molecule is incorrect, hover over each octagon to see each error</div>
+        <div className="text-box" id="page-nine-box">x means your molecule is incorrect, hover over each octagon to see each error</div>
         
         <div className="text-box" id="check-box"> key: 
         <div className="check-row"><img src={appleCheck} alt="check box" className="tut-icon"/> valid</div>
