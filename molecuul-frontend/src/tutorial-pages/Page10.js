8450347f8bef410917365ab0954abbe9c1bbc973
Daniel-Astriddao <daniel@astriddao.xyz>
2022-12-02 11:29:26 -0800

    fixed wording

--- molecuul-frontend/src/tutorial-pages/Page10.js
+++ molecuul-frontend/src/tutorial-pages/Page10.js
@@ -12,7 +12,7 @@ function PageTen (props) {
     return (
         
         <div className="pages">
-        <div className="text-box" id="page-nine-box">x means you molecule is incorrect, hover over each octagon to see each error</div>
+        <div className="text-box" id="page-nine-box">x means you molecule is incorrect, hover over an element to see each error</div>
         
         <div className="text-box" id="check-box"> key: 
         <div className="check-row"><img src={appleCheck} alt="check box" className="tut-icon"/> valid</div>
